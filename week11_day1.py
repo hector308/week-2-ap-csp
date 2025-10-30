ea3dc5a5ab94e05f57cda0da7620f989a0b2220d
hector308 <hecotrm2344@gmail.com>
2025-10-30 13:50:23 +0000

    lol

--- week11_day1.py
+++ week11_day1.py
@@ -29,8 +29,12 @@ print(favorite_number+num1)
 
 #  Step 4: User Input Practice
 # Ask the user two questions and combine answers
-num3= int(input("What is your favorite number?"))
-num4= int(input("What is your second favorite number?"))
-print (num3+num4)
+#num3= int(input("What is your favorite number?"))
+#num4= int(input("What is your second favorite number?"))
+#print (num3+num4)
 # ⚙️ Step 5: Final Challenge (combine it all)
 # Use math and strings together
+username= (input("What is your name?"))
+favnum1= int(input("What is your favorite number"))
+favnum2= int(input("What is your second favorite number"))
+print(f"Hello {username}, how are you, I know you like the color{favnum1}+{favnum2}")
